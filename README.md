c16fa4c215d337888815097f0cba4b2b7baabd24
redpt1 <397240017@qq.com>
2023-02-09 16:02:34 +0800

    uniswap
    前两步完成，差最后两步，但是还需测试

--- README.md
+++ README.md
@@ -2,4 +2,10 @@
 
 aave： 只要追踪flashloan事件即可\
 dydx： 四个事件按顺序即可\
-uniswap: 没看懂，下回再说
+uniswap: \
+1.找PairCreate中的对合约地址（提供swap函数的）\
+         2.在对合约中找触发swap事件的tx\
+         3.判断tx：\
+            1）调用swap函数时，data是否大于0\
+            2）由uniswapV2Call触发的内部事务必须带哦用transfer or transferFrom函数\
+            3）transfer，transferFrom函数地址为对合约地址
--- uniswapV2.py
+++ uniswapV2.py
@@ -1,53 +1,52 @@
 # address:0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f
+import csv
+
 from eth_utils import keccak
 
 import pandas as pd
 
 # function signature
 STEP_1 = '0x' + keccak(b'PairCreated(address,address,address,uint256)').hex()
-STEP_3 = '0x' + keccak(b'LogCall(address,uint256,address)').hex()
-STEP_4 = '0x' + keccak(b'LogDeposit(address,uint256,uint256,tuple,address)').hex()
+STEP_2 = '0x' + keccak(b'Swap(address,uint256,uint256,uint256,uint256,address)').hex()
 
+STEP_3_1 = '0x' + keccak(b'swap(uint256,uint256,address,bytes)').hex()[:8]
+STEP_3_2_1 =  '0x' + keccak(b'transfer(address,uint256)').hex()[:8]
+STEP_3_2_2 = '0x' + keccak(b'transferFrom(address,address,uint256)').hex()[:8]
 
 # FLASH_1 = '0x' + keccak(b'Flash(address,address,uint256,uint256,uint256,uint256)').hex()
 
 
-
 def search_uniswap():
-    csv_reader = pd.read_csv('E:/dataset/logs.csv').groupby('transaction_hash')
-    num_of_dydx = 0
-    for tx, group in csv_reader:
-        log_list = []
-        for row in group['topics']:
-            log_list.append(row.split(',')[0])
-        if len(log_list) >= 4:
-            if STEP_1 in log_list and STEP_2 in log_list and STEP_3 in log_list and STEP_4 in log_list:
-                index_1 = log_list.index(STEP_1)
-                index_2 = log_list.index(STEP_2)
-                index_3 = log_list.index(STEP_3)
-                index_4 = log_list.index(STEP_4)
-                if index_1 < index_2 < index_3 < index_4:
-                    num_of_dydx += 1
-                    print(list(group))
+    csv_reader = pd.read_csv('E:/dataset/logs.csv')
+    # 查找全部对合约地址
+    pair_contract_address = []
+    for index, row in csv_reader.iterrows():
+        if STEP_1 == row['topics'].split(',')[0]:
+            raw_address = '0x' + row['data'][26:66]
+            pair_contract_address.append(raw_address)
 
-    print("total: %d" % num_of_dydx)
+    # 根据对合约地址聚合
+    csv_group_by = csv_reader.groupby('address')
+    # 获取触发swap事件的tx hash
+    emit_swap_tx_hash = []
+    for tx, group in csv_group_by:
+        if tx in pair_contract_address:
+            for index, row in group.iterrows():
+                # 如果触发了swap事件
+                if row['topics'].split(',')[0] == STEP_2:
+                    emit_swap_tx_hash.append(row['transaction_hash'])
+        else:
+            continue
+    # 检查tx在调用对合约中swap函数时data是否大于0
+    csv_reader_tx = pd.read_csv('E:/dataset/transactions.csv')
+    for tx, row in csv_reader_tx.iterrows():
+        if row['hash'] in emit_swap_tx_hash:
+            if STEP_3_1 == row['input'][:10]:
+                # 总长度减去data数据部分的偏移量（字节）如果不足64位（32字节）就不是，移除
+                if len(row['input']) - (10 + 4 * 64) <= 64:
+                    emit_swap_tx_hash.remove(row['hash'])
 
-    # num_of_p1 = 0
-    # num_of_p2 = 0
-    # num_of_p3 = 0
-    # for index, row in csv_reader.iterrows():
-    #     if PATTERN_1 == row['topics'].split(',')[0]:
-    #         num_of_p1 += 1
-    #         print(row)
-    #     elif PATTERN_2 == row['topics'].split(',')[0]:
-    #         num_of_p2 += 1
-    #         print(row)
-    #     elif PATTERN_3 == row['topics'].split(',')[0]:
-    #         num_of_p3 += 1
-    #         print(row)
-    #
-    # print("v1: %d, v2: %d, v3: %d, total: %d" % (num_of_p1, num_of_p2, num_of_p3, num_of_p1 + num_of_p2 + num_of_p3))
-
+    #TODO 最后两步判断
 
 if __name__ == '__main__':
-    search_dydx()
+    search_uniswap()
